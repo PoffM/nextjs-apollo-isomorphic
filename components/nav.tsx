dce65cca25d1ce3a465db6dba17e631940ad6d75
poffm <matp@live.ca>
2019-05-31 22:51:28 -0400

    Removed nav links

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,36 +1,7 @@
 import React from "react";
-import Link from "next/link";
-
-const links = [
-  {
-    href: "https://github.com/segmentio/create-next-app",
-    label: "Github",
-    key: null
-  }
-].map(link => {
-  link.key = `nav-link-${link.href}-${link.label}`;
-  return link;
-});
 
 const Nav = () => (
   <nav>
-    <ul>
-      <li>
-        <Link prefetch href="/">
-          <a>Home</a>
-        </Link>
-      </li>
-      <ul>
-        {links.map(({ key, href, label }) => (
-          <li key={key}>
-            <Link href={href}>
-              <a>{label}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </ul>
-
     <style jsx>{`
       :global(body) {
         margin: 0;
