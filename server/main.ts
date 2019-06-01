40a5939260fa62887c5fb2556e0924fe546ac8bf
poffm <matp@live.ca>
2019-05-31 22:09:30 -0400

    Added custom port via env variable PORT

--- server/main.ts
+++ server/main.ts
@@ -4,7 +4,7 @@ import next from "next";
 import { createApolloServer } from "./createApolloServer";
 
 (async () => {
-  const port = 3000;
+  const port = process.env.PORT || 3000;
   const dev = process.env.NODE_ENV !== "production";
 
   const nextApp = next({ dev });
